9a645efa08583383ede2067fdaf8851736281ac3
sjie <ligongdamao@163.com>
2020-06-11 17:42:41 +0800

    proxy design

---
+++ design-patterns/src/main/java/cn/yusite/basic/tutorial/design/structural/proxy/ThirdPartyYouTubeLib.java
@@ -0,0 +1,9 @@
+package cn.yusite.basic.tutorial.design.structural.proxy;
+
+import java.util.HashMap;
+
+public interface ThirdPartyYouTubeLib {
+    HashMap<String, Video> popularVideos();
+
+    Video getVideo(String videoId);
+}
