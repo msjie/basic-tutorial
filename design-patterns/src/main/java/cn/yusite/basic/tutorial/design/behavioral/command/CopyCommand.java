85fcd961b9d4b9b987f9e1d30dd74934d98d90e0
sjie <ligongdamao@163.com>
2020-06-11 18:56:27 +0800

    behavioral command design

---
+++ design-patterns/src/main/java/cn/yusite/basic/tutorial/design/behavioral/command/CopyCommand.java
@@ -0,0 +1,15 @@
+package cn.yusite.basic.tutorial.design.behavioral.command;
+
+public class CopyCommand extends Command {
+
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+
+}
